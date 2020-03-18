8caa2068fb3a717681703b901eebd44c78fe35bb
liao1a <153887111@qq.com>
2020-03-18 09:46:50 +0800

    增加配置项注释说明

--- back/config/app.js
+++ back/config/app.js
@@ -1,6 +1,6 @@
 let appConfig = {
-  name: process.env.TMS_APP_NAME || "tms-mongodb-web",
-  port: process.env.TMS_APP_PORT || 3000,
+  name: process.env.TMS_APP_NAME || "tms-mongodb-web", // 如需自定义可在项目根目录下创建/back/.env文件配置成环境变量
+  port: process.env.TMS_APP_PORT || 3000, // 如需自定义可在项目根目录下创建/back/.env文件配置成环境变量
   router: {
     auth: {
       prefix: 'auth' // 鉴权接口调用url的前缀
@@ -20,7 +20,7 @@ let appConfig = {
       privateKey: "tms-mongodb-web",
       expiresIn: 3600
     },
-    // 鉴权 redis  如果鉴权信息要从redis中获取，可在此连接redis需注释上面jwt对象，redis中端口、地址等信息可在启动目录下.env文件中配置成环境变量
+    // 鉴权 redis  如果鉴权信息要从redis中获取，可在此连接redis需注释上面jwt对象，redis中端口、地址等信息可在项目根目录下创建/back/.env文件配置成环境变量
     // redis: {
     //   prefix: process.env.TMS_REDIS_PREFIX || "tms-mongodb-web",
     //   host: process.env.TMS_REDIS_HOST || "localhost",
@@ -29,15 +29,15 @@ let appConfig = {
     // },
     // 验证码
     captcha: {
-      // 如需自定义验证码，可在启动目录下创建生成验证码文件，将文件路径命名给path并注释code
-      // path: "/auth/captcha.js"
+      // 如需自定义验证码，可在项目根目录下创建生成验证码文件，将文件路径命名给path并注释code
+      // path: "files"
       code: "1234"
     },
     // 
     client: {
-      // 如需自定义鉴权函数，可在启动目录下创建鉴权函数, 将文件路径命名给path并注释 accounts
-      // path: "/auth/client.js"
-      accounts: [
+      // 如需自定义鉴权函数，可在项目根目录下创建鉴权函数, 将文件路径命名给path并注释 accounts
+      // path: "files"
+      accounts: [ // 默认用户组
         {
           id: 1,
           username: "root",
