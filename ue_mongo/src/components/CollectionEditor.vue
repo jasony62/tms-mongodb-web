6aceb21569c1ecae5fbe4575774860a6d6e844b3
aYellowApple <2764411477@qq.com>
2020-03-25 19:17:33 +0800

    modify wenjian to wendang

--- ue_mongo/src/components/CollectionEditor.vue
+++ ue_mongo/src/components/CollectionEditor.vue
@@ -5,18 +5,18 @@
     :close-on-click-modal="closeOnClickModal"
   >
     <el-form ref="form" :model="collection" label-position="top">
-      <el-form-item label="文件名称（英文）">
+      <el-form-item label="文档名称（英文）">
         <el-input v-model="collection.name"></el-input>
       </el-form-item>
-      <el-form-item label="文件显示名（中文）">
+      <el-form-item label="文档显示名（中文）">
         <el-input v-model="collection.title"></el-input>
       </el-form-item>
-      <el-form-item label="文件列定义">
+      <el-form-item label="文档列定义">
         <el-select placeholder="请选择" v-model="collection.schema_id" clearable filterable>
           <el-option v-for="item in schemas" :key="item._id" :label="item.title" :value="item._id"></el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="文件拓展属性（选填）">
+      <el-form-item label="文档拓展属性（选填）">
         <el-select placeholder="请选择" v-model="collection.extensionInfo.schemaId" clearable filterable>
           <el-option v-for="item in extensions" :key="item._id" :label="item.title" :value="item._id"></el-option>
         </el-select>
