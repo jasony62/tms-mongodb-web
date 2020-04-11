213f7c5cdac1d6786378a0103c25b9566a1a9f13
aYellowApple <2764411477@qq.com>
2020-04-11 16:31:53 +0800

    vueconfigjs add xiegang

--- ue_mongo/vue.config.js
+++ ue_mongo/vue.config.js
@@ -1,8 +1,8 @@
 const VUE_APP_BASE_URL = process.env.VUE_APP_BASE_URL ? process.env.VUE_APP_BASE_URL : ''
 
 module.exports = {
-  publicPath: `${VUE_APP_BASE_URL}`,
-  outputDir: `dist${VUE_APP_BASE_URL}`,
+  publicPath: `${VUE_APP_BASE_URL}/`,
+  outputDir: `dist${VUE_APP_BASE_URL}/`,
 	pages: {
 		index: {
 				entry: "src/main.js",
