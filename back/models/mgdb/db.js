dddf76555a4f637dc8de1fed8e63857569432468
liao1a <153887111@qq.com>
2020-05-11 21:13:35 +0800

    update

--- back/models/mgdb/db.js
+++ back/models/mgdb/db.js
@@ -6,8 +6,8 @@ class Db extends Base {
    */
   _checkDbName(dbName) {
     //格式化库名
-    if (new RegExp('^[a-zA-Z]+[0-9a-zA-Z_]{0,9}$').test(dbName) !== true)
-      return [false, '库名必须以小写英文字母开头，可与小写英文字母或_或数字组合，且最长64位']
+    if (new RegExp('^[a-zA-Z]+[0-9a-zA-Z_]{0,63}$').test(dbName) !== true)
+      return [false, '库名必须以英文字母开头，可与英文字母或_或数字组合，且最长64位']
 
     // 查询库名是否是mongodb自带数据库
     if (['admin', 'config', 'local'].includes(dbName))
