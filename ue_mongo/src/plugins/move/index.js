225482fca01e3ce332e7fc31608802392d2cb3da
liuzhiwei <liuzhiwei@ctsi.com.cn>
2020-11-26 10:31:47 +0800

    删除无关代码

--- ue_mongo/src/plugins/move/index.js
+++ ue_mongo/src/plugins/move/index.js
@@ -3,16 +3,5 @@ import { TmsAxios } from 'tms-vue'
 const base = (process.env.VUE_APP_BACK_API_BASE || '') + '/mongo/document'
 
 export default {
-  list(olddbName, oldclName, ruleDbName, ruleClName) {
-    return TmsAxios.ins('mongodb-api')
-      .get(`${base}/getDocsByRule?db=${olddbName}&cl=${oldclName}&ruleDb=${ruleDbName}&ruleCl=${ruleClName}`)
-      .then(rst => rst.data.result)
-      .catch(err => Promise.reject(err))
-  },
-  movebyRule(newdbName, newclName, olddbName, oldclName, ruleDbName, ruleClName, transforms, docIds) {
-    return TmsAxios.ins('mongodb-api')
-      .post(`${base}/moveByRule?newDb=${newdbName}&newCl=${newclName}&oldDb=${olddbName}&oldCl=${oldclName}&ruleDb=${ruleDbName}&ruleCl=${ruleClName}&markResultColumn=import_status&transforms=${transforms}`, docIds)
-      .then(rst => rst.data.result)
-      .catch(err => Promise.reject(err))
-  }
+  
 }
