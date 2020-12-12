cb54209331129461802cd27aa13579d6e1041ba8
jasony62 <jasony62@qq.com>
2020-12-12 14:17:26 +0800

    清理代码

--- init_replicate.sh
+++ init_replicate.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 
-# 复制配置文件到容器中
-# docker cp ./init_replicate.js tms-mongodb-mongo:/home/int_replicate.js
-
-# 按照配置文件初始化复制集
-# docker exec -it tms-mongodb-mongo mongo --port 37017 /home/int_replicate.js
-
 # 配置复制集
 docker exec -it tms-mongodb-mongo sh /home/init_replicate.sh
