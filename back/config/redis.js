994658995c9e0b98046c0d734aef880d778895be
b-ss <2764411477@qq.com>
2021-06-21 14:48:50 +0800

    0621reids支持配置密码

--- back/config/redis.js
+++ back/config/redis.js
@@ -3,5 +3,6 @@ module.exports = {
   prefix: process.env.TMS_REDIS_PREFIX || 'tms-mongodb-web',
   host: process.env.TMS_REDIS_HOST || '127.0.0.1',
   port: parseInt(process.env.TMS_REDIS_PORT) || 6379,
+  password: process.env.TMS_REDIS_PWD || '',
   expiresIn: parseInt(process.env.TMS_REDIS_EXPIRESIN) || 7200
 }
