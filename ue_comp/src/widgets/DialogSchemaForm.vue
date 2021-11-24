0f8c59b76fa1fb8804bbb1a37a80172184004551
b-ss <2764411477@qq.com>
2021-11-24 14:59:02 +0800

    按照新方式取token值

--- ue_comp/src/widgets/DialogSchemaForm.vue
+++ ue_comp/src/widgets/DialogSchemaForm.vue
@@ -46,7 +46,7 @@ const ComponentOptions = {
     onFileDownload(name, url) {
       /**需要访问控制 */
       if (process.env.VUE_APP_BACK_AUTH_BASE) {
-        const accessToken = sessionStorage.getItem('access_token')
+        const accessToken = this.$getToken()
         window.open(`${url}?access_token=${accessToken}`)
       } else {
         window.open(`${url}`)
