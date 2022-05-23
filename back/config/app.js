d577e3c625da88fdb24ff60b8f8b317fb39ae9ca
wangqian <193614106@qq.com>
2022-05-24 00:30:22 +0800

    fix:接口调试

--- back/config/app.js
+++ back/config/app.js
@@ -3,10 +3,11 @@ let appConfig = {
   port: process.env.TMS_APP_PORT || 3000, // 如需自定义可在项目根目录下创建/back/.env文件配置成环境变量
   router: {
     auth: {
-      prefix: process.env.TMS_APP_ROUTER_AUTH || '', // 鉴权接口调用url的前缀
+      prefix: process.env.TMS_APP_ROUTER_AUTH || 'auth', // 鉴权接口调用url的前缀
     },
     controllers: {
       prefix: process.env.TMS_APP_ROUTER_CONTROLLER || 'api', // 控制器接口调用url的前缀，例如：/api
+      plugins_npm: [{ id: 'tms-koa-account', alias: 'account' }], // 账号管理相关接口
     },
     plugins: {
       prefix: process.env.TMS_APP_ROUTER_PLUGIN || 'plugin', // 插件接口调用url的前缀
@@ -33,26 +34,35 @@ let appConfig = {
     // 验证码
     captcha: {
       // 如需自定义验证码，可在项目根目录下创建生成验证码文件，将文件路径命名给path并注释code
-      // path: "files"
-      code: '1234',
+      // path: '/auth/captcha.js', // 指定验证码实现方法
+      // checkPath: '/auth/checkPath.js' // 指定检查验证码方法
+      // code: '1234', // 指定固定验证码
+      npm: {
+        disabled: false,
+        id: 'tms-koa-account',
+        module: 'models/captcha',
+        checker: 'checkCaptcha',
+        generator: "createCaptcha"
+      },
     },
     //
     client: {
       // 如需自定义鉴权函数，可在项目根目录下创建鉴权函数, 将文件路径命名给path并注释 accounts
-      // path: "files"
-      accounts: [
-        // 默认用户组
-        {
-          id: 1,
-          username: 'root',
-          password: 'root',
-        },
-        {
-          id: 2,
-          username: 'user1',
-          password: 'user1',
-        },
-      ],
+      // path: '/auth/client.js', // 指定用户认证实现方法
+      // registerPath: "/auth/register.js" // 指定用户注册的实现方法
+      // accounts: [ // 默认用户组
+      //   {
+      //     id: 1,
+      //     username: 'root',
+      //     password: 'root',
+      //   },
+      // ],
+      npm: {
+        disabled: false,
+        id: 'tms-koa-account',
+        authentication: 'models/authenticate',
+        register: 'models/register',
+      },
     },
   },
   tmwConfig: {
