b66cb1fb09c61cdfb6f04438ad5b7f1e5eed2270
jasony62 <jasony62@qq.com>
2022-06-07 13:30:57 +0800

    更新依赖包，优化代码

--- ue_admin2/src/components/editor/PickFile.vue
+++ ue_admin2/src/components/editor/PickFile.vue
@@ -1,34 +1,19 @@
 <template>
-  <el-dialog
-    title="选取文件"
-    v-model="innerVisible"
-    :fullscreen="true"
-    :destroy-on-close="true"
-    :close-on-click-modal="true"
-    :before-close="onBeforeClose"
-  >
-    <iframe
-      id="iframe"
-      width="100%"
-      height="100%"
-      frameborder="0"
-      :src="url"
-      marginwidth="0"
-      marginheight="0"
-      scrolling="no"
-      @load="iframeLoad"
-    ></iframe>
+  <el-dialog title="选取文件" v-model="innerVisible" :fullscreen="true" :destroy-on-close="true"
+    :close-on-click-modal="true" :before-close="onBeforeClose">
+    <iframe id="iframe" width="100%" height="100%" frameborder="0" :src="url" marginwidth="0" marginheight="0"
+      scrolling="no" @load="iframeLoad"></iframe>
   </el-dialog>
 </template>
 
 <script setup lang="ts">
-import { PropType, ref, onMounted, nextTick } from 'vue'
+import { onMounted, nextTick } from 'vue'
 import { parseLocation } from '@/global'
 
 const props = defineProps({
   innerVisible: { default: true },
-  url: { type: String },
-  onClose: { type: Function, default: (newDoc: any) => {} },
+  url: { type: String, required: true },
+  onClose: { type: Function, default: (newDoc: any) => { } },
 })
 
 const { onClose } = props
@@ -43,7 +28,7 @@ const onBeforeClose = () => {
 }
 
 const iframeLoad = () => {
-  var ifm = document.getElementById('iframe')
+  let ifm: any = document.getElementById('iframe')
   ifm.height = document.documentElement.clientHeight
 }
 
@@ -51,9 +36,9 @@ onMounted(() => {
   nextTick(() => {
     window.addEventListener(
       'message',
-      (e) => {
-        const origin = event.origin || event.originalEvent.origin
-        const locationObj =  parseLocation(props.url)     
+      (e: any) => {
+        const origin = e.origin || e.originalEvent.origin
+        const locationObj = parseLocation(props.url)
         if (origin === locationObj.origin) {
           closeDialog(e.data)
         }
