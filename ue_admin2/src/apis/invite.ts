b1603be9871421d155c69c3d2101052ed056a6e7
jasony62 <jasony62@qq.com>
2022-06-29 18:01:09 +0800

    更新代码

--- ue_admin2/src/apis/invite.ts
+++ ue_admin2/src/apis/invite.ts
@@ -1,25 +1,26 @@
 import { TmsAxios } from 'tms-vue3'
 
-const base = (import.meta.env.VITE_BACK_API_BASE || '') + '/admin/bucket/coworker'
+const base =
+  (import.meta.env.VITE_BACK_API_BASE || '') + '/admin/bucket/coworker'
 type ApiRst = {
   data: { result: any }
 }
 export default {
-  accept(bucket:any, nickname:string, code:any) {
+  accept(bucket: any, nickname: string, code: any) {
     const params = { nickname, code }
     return TmsAxios.ins('mongodb-api')
       .post(`${base}/accept?bucket=${bucket}`, params)
-      .then((rst:ApiRst) => rst.data.result)
+      .then((rst: ApiRst) => rst.data.result)
   },
-  invite(bucket:any, nickname:string) {
+  invite(bucket: any, nickname: string) {
     const params = { nickname }
     return TmsAxios.ins('mongodb-api')
       .post(`${base}/invite?bucket=${bucket}`, params)
-      .then((rst:ApiRst) => rst.data.result)
+      .then((rst: ApiRst) => rst.data.result)
   },
-  remove(bucket:any, nickname:string) {
+  remove(bucket: any, nickname: string) {
     return TmsAxios.ins('mongodb-api')
       .get(`${base}/remove?bucket=${bucket}&coworker=${nickname}`)
-      .then((rst:ApiRst) => rst.data.result)
+      .then((rst: ApiRst) => rst.data.result)
   },
 }
