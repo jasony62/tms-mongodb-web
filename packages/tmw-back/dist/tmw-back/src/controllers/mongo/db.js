42f59584991d8b23f9ec099f837734df79245463
jasony62 <jasony62@qq.com>
2022-07-05 18:10:31 +0800

    更新代码

--- packages/tmw-back/dist/tmw-back/src/controllers/mongo/db.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dbBase_1 = require("../dbBase");
-class Db extends dbBase_1.default {
-    constructor(...args) {
-        super(...args);
-    }
-}
-exports.default = Db;
