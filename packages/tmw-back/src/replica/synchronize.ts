1adca3547d745907e2629334813e56e2c586240a
quhua <quhuame@163.com>
2022-07-06 17:40:56 +0800

    更新代码

--- packages/tmw-back/src/replica/synchronize.ts
+++ packages/tmw-back/src/replica/synchronize.ts
@@ -4,7 +4,7 @@
 import * as log4js from 'log4js'
 import * as path from 'path'
 import * as fs from 'fs'
-let cnfpath = path.resolve(process.cwd()+'/config/log4js.ts')
+let cnfpath = path.resolve(process.cwd()+'/config/log4js.js')
 if (fs.existsSync(cnfpath)) {
   const log4jsConfig = require(process.cwd()+'/config/log4js')
   log4js.configure(log4jsConfig)
