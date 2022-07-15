a75eff949bc28dae32fd1699b59911bedf9fb2ae
jasony62 <jasony62@qq.com>
2022-07-15 15:25:56 +0800

    优化schema编辑框

--- ue_admin2/src/components/editor/SchemaEditor.vue
+++ ue_admin2/src/components/editor/SchemaEditor.vue
@@ -1,32 +1,44 @@
 <template>
   <el-dialog title="文档内容定义" v-model="dialogVisible" :fullscreen="true" :destroy-on-close="true"
     :close-on-click-modal="false" :before-close="onBeforeClose">
-    <div class="editor">
+    <div class="h-full flex flex-col">
       <el-tabs v-model="activeTab" type="card">
         <el-tab-pane label="基本信息" name="first"></el-tab-pane>
         <el-tab-pane label="列定义" name="second"></el-tab-pane>
       </el-tabs>
-      <el-form v-show="activeTab === 'first'" :model="schema" label-position="top">
-        <el-form-item label="显示名（中文）">
-          <el-input v-model="schema.title"></el-input>
-        </el-form-item>
-        <el-form-item label="说明">
-          <el-input type="textarea" v-model="schema.description"></el-input>
-        </el-form-item>
-        <el-form-item label="标签">
-          <el-select v-model="schema.tags" multiple clearable placeholder="请选择">
-            <el-option v-for="tag in tags" :key="tag._id" :label="tag.name" :value="tag.name"></el-option>
-          </el-select>
-        </el-form-item>
-      </el-form>
-      <tms-json-schema ref="$jse" v-show="activeTab === 'second'" :schema="schema.body" :root-name="'$'"
-        :on-upload="onUploadFile" class="schema-editor">
-        <template #extattrs="{ attrs }">
-          <el-form-item label="不可修改">
-            <el-switch v-model="attrs.readonly"></el-switch>
+      <div class="overflow-auto flex-grow">
+        <el-form v-show="activeTab === 'first'" :model="schema" label-position="top">
+          <el-form-item label="显示名（中文）">
+            <el-input v-model="schema.title"></el-input>
           </el-form-item>
-        </template>
-      </tms-json-schema>
+          <el-form-item label="说明">
+            <el-input type="textarea" v-model="schema.description"></el-input>
+          </el-form-item>
+          <el-form-item label="标签">
+            <el-select v-model="schema.tags" multiple clearable placeholder="请选择">
+              <el-option v-for="tag in tags" :key="tag._id" :label="tag.name" :value="tag.name"></el-option>
+            </el-select>
+          </el-form-item>
+        </el-form>
+        <div v-show="activeTab === 'second'" class="flex flex-row gap-4 h-full overflow-auto">
+          <tms-json-schema ref="$jse" :schema="schema.body" :root-name="'$'" :on-upload="onUploadFile"
+            class="h-full overflow-auto">
+            <template #extattrs="{ attrs }">
+              <el-form-item label="不可修改">
+                <el-switch v-model="attrs.readonly"></el-switch>
+              </el-form-item>
+            </template>
+          </tms-json-schema>
+          <div class="h-full flex-grow flex flex-col gap-2 relative">
+            <div class="absolute top-0 right-0">
+              <el-button @click="preview">预览</el-button>
+            </div>
+            <div class="border-2 border-gray-300 rounded-md p-2 h-full overflow-auto">
+              <pre>{{ previewResult }}</pre>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
     <template #footer>
       <el-button type="primary" @click="onSubmit">提交</el-button>
@@ -64,6 +76,8 @@ const { onClose, bucketName } = props
 const activeTab = ref('first')
 const tags = ref([] as any[])
 
+const previewResult = ref('')
+
 // 关闭对话框时执行指定的回调方法
 const closeDialog = (newSchema?: any) => {
   onClose(newSchema)
@@ -92,6 +106,10 @@ const onUploadFile = (file: any) => {
     .catch((error: any) => {
       throw error
     })
+}
+
+const preview = () => {
+  previewResult.value = JSON.stringify($jse.value?.editing(), null, 2)
 }
 
 const onSubmit = () => {
@@ -128,8 +146,5 @@ const onSubmit = () => {
     }
   }
 
-  .editor {
-    height: 100%;
-  }
 }
 </style>
