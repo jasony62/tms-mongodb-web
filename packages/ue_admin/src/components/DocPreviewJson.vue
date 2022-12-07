102bf5b8c789c3e932f5ba95b1664cac274eb759
jasony62 <jasony62@qq.com>
2022-12-07 13:39:01 +0800

    文档预览功能支持导入json文件

--- packages/ue_admin/src/components/DocPreviewJson.vue
+++ packages/ue_admin/src/components/DocPreviewJson.vue
@@ -2,12 +2,19 @@
   <el-drawer v-model="opened" size="50%" :with-header="false" :show-close="false" :close-on-click-modal="true"
     :destroy-on-close="true">
     <div class="h-full w-full flex flex-col">
-      <el-form class="flex-none">
-        <el-form-item>
+      <div class="flex-none flex flex-row gap-2">
+        <div>
           <el-button @click="onClose">关闭</el-button>
+        </div>
+        <div>
           <el-button @click="onSave" v-if="SupportSave">保存修改</el-button>
-        </el-form-item>
-      </el-form>
+        </div>
+        <el-upload ref="uploadRef" :auto-upload="false" :show-file-list="false" :on-change="onImportFile">
+          <template #trigger>
+            <el-button>导入文件</el-button>
+          </template>
+        </el-upload>
+      </div>
       <div class="flex-grow">
         <div ref="elJsonEditor" class="w-full h-full"></div>
       </div>
@@ -30,6 +37,7 @@ import JSONEditor from 'jsoneditor'
 import 'jsoneditor/dist/jsoneditor.css'
 import { useDocPreviewJson } from '@/composables/docPreviewJson'
 import { computed, nextTick, ref, watch } from 'vue'
+import { ElMessage, UploadFile } from 'element-plus'
 
 const { opened, previewDoc, cbSave } = useDocPreviewJson()
 
@@ -58,12 +66,36 @@ watch(opened, (val) => {
     })
   }
 })
-
+/**
+ * 导入文件
+ * @param uploadFile
+ */
+const onImportFile = (uploadFile: UploadFile) => {
+  if (uploadFile?.raw) {
+    const reader = new FileReader()
+    reader.addEventListener('load', () => {
+      if (reader.result) {
+        try {
+          let newDoc = JSON.parse(reader.result.toString())
+          jsonEditor.set(newDoc)
+        } catch (e: any) {
+          ElMessage.success({ showClose: true, type: 'error', message: '导入失败，原因：' + e.message })
+        }
+      }
+    })
+    reader.readAsText(uploadFile.raw)
+  }
+}
+/**
+ * 关闭
+ */
 const onClose = () => {
   opened.value = false
   jsonEditor = null
 }
-
+/**
+ * 保存
+ */
 const onSave = () => {
   if (jsonEditor && typeof cbSave.value === 'function') {
     let newDoc = jsonEditor.get()
