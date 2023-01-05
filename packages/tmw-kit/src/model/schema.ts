71ac6bd166112a2ebf599dc1c092a7f4c0a49ea5
jasony62 <jasony62@qq.com>
2023-01-05 21:53:31 +0800

    优化代码

--- packages/tmw-kit/src/model/schema.ts
+++ packages/tmw-kit/src/model/schema.ts
@@ -5,13 +5,13 @@ const ObjectId = mongodb.ObjectId
 class Schema extends Base {
   /**
    * 根据ID获得字段定义
-   * @param {object} tmwCl
    */
   async bySchemaId(id, { onlyProperties = true } = {}) {
     let query = {
       _id: new ObjectId(id),
       type: 'schema',
     }
+
     return this.clMongoObj.findOne(query).then((schema) => {
       if (onlyProperties === true) {
         if (!schema) return false
@@ -24,10 +24,25 @@ class Schema extends Base {
    * 根据名称获得字段定义
    * @param {string} name
    */
-  async byName(name: string, { onlyProperties = true, dbName = null } = {}) {
-    let query: any = { name, type: 'schema' }
-    if (this.bucket) query.bucket = this.bucket.name
-    if (dbName) query['db.name'] = dbName
+  async byName(
+    name: string,
+    { onlyProperties = true, dbName = null, scope = 'document' } = {}
+  ) {
+    let query: any // 查询条件
+    if (dbName) {
+      query = {
+        $and: [
+          { type: 'schema', name, scope },
+          {
+            $or: [{ 'db.name': dbName }, { db: null }],
+          },
+        ],
+      }
+      if (this.bucket) query['$and'].push({ bucket: this.bucket.name })
+    } else {
+      query = { type: 'schema', name, scope, db: null }
+      if (this.bucket) query.bucket = this.bucket.name
+    }
 
     return this.clMongoObj.findOne(query).then((schema) => {
       if (onlyProperties === true) {
