3b5ea35fc242bc9302de212205a9baa25dd0a3ba
jasony62 <jasony62@qq.com>
2023-01-13 14:12:00 +0800

    优化代码

--- packages/tmw-kit/src/model/plugin/context.ts
+++ packages/tmw-kit/src/model/plugin/context.ts
@@ -67,58 +67,62 @@ export class Context {
         let files: string[] = glob.sync(`${absDir}/*.js`)
         for (let file of files) {
           if (file.indexOf('node_modules') !== -1) continue
-          let { createPlugin } = require(file)
-          if (typeof createPlugin !== 'function') {
-            logger.warn(
-              `插件文件[${path.basename(
-                file
-              )}]不可用，没有导出[createPlugin]方法`
-            )
-            continue
-          }
-
-          // 支持用1个插件文件创建多个插件实例
-          let plugin: PluginProfile | boolean = createPlugin(file)
-          if (false === plugin) {
-            logger.warn(`插件文件[${file}]创建插件失败`)
-            continue
-          }
-
-          let plugins = Array.isArray(plugin) ? plugin : [plugin]
-          for (let onePlugin of plugins) {
-            // if (!onePlugin || !(onePlugin instanceof PluginBase)) {
-            //   logger.warn(`插件文件[${file}]不是[PluginBase]类型，不可用`)
-            //   logger.debug(`插件文件[${file}]`, onePlugin)
-            //   continue
-            // }
-            if (onePlugin.disabled === true) {
-              logger.warn(`插件文件[${onePlugin.file}]已禁用`)
+          try {
+            let { createPlugin } = require(file)
+            if (typeof createPlugin !== 'function') {
+              logger.warn(
+                `插件文件[${path.basename(
+                  file
+                )}]不可用，没有导出[createPlugin]方法`
+              )
               continue
             }
-            let passed = await validatePlugin(onePlugin)
-            if (passed) {
-              if (_pluginsByName.has(onePlugin.name)) {
-                logger.warn(
-                  `插件文件[${onePlugin.file}]不可用，已有同名插件[name=${onePlugin.name}]`
-                )
+
+            // 支持用1个插件文件创建多个插件实例
+            let plugin: PluginProfile | boolean = createPlugin(file)
+            if (false === plugin) {
+              logger.warn(`插件文件[${file}]创建插件失败`)
+              continue
+            }
+
+            let plugins = Array.isArray(plugin) ? plugin : [plugin]
+            for (let onePlugin of plugins) {
+              // if (!onePlugin || !(onePlugin instanceof PluginBase)) {
+              //   logger.warn(`插件文件[${file}]不是[PluginBase]类型，不可用`)
+              //   logger.debug(`插件文件[${file}]`, onePlugin)
+              //   continue
+              // }
+              if (onePlugin.disabled === true) {
+                logger.warn(`插件文件[${onePlugin.file}]已禁用`)
                 continue
               }
-              let { name, scope } = onePlugin
-              switch (scope) {
-                case 'database':
-                  _dbProfiles.push(onePlugin.profile)
-                  break
-                case 'collection':
-                  _clProfiles.push(onePlugin.profile)
-                  break
-                case 'document':
-                  _docProfiles.push(onePlugin.profile)
-                  break
-              }
-              _pluginsByName.set(name, onePlugin)
+              let passed = await validatePlugin(onePlugin)
+              if (passed) {
+                if (_pluginsByName.has(onePlugin.name)) {
+                  logger.warn(
+                    `插件文件[${onePlugin.file}]不可用，已有同名插件[name=${onePlugin.name}]`
+                  )
+                  continue
+                }
+                let { name, scope } = onePlugin
+                switch (scope) {
+                  case 'database':
+                    _dbProfiles.push(onePlugin.profile)
+                    break
+                  case 'collection':
+                    _clProfiles.push(onePlugin.profile)
+                    break
+                  case 'document':
+                    _docProfiles.push(onePlugin.profile)
+                    break
+                }
+                _pluginsByName.set(name, onePlugin)
 
-              logger.info(`从文件[${file}]创建插件[name=${name}]`)
+                logger.info(`从文件[${file}]创建插件[name=${name}]`)
+              }
             }
+          } catch (e) {
+            logger.error(`从文件[${file}]创建插件发生异常`, e)
           }
         }
       })
