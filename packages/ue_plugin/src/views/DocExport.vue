20d4b1e2ae8061c52dbfd7c39c09f9f800209944
baiss <2764411477@qq.com>
2023-02-23 09:59:34 +0800

    修改导出提示文字

--- packages/ue_plugin/src/views/DocExport.vue
+++ packages/ue_plugin/src/views/DocExport.vue
@@ -10,8 +10,8 @@
         </el-form-item>
         <el-form-item label="保存方式" v-if="outType === 'json'">
           <el-radio-group v-model="outAmount" class="ml-4">
-            <el-radio label="one" size="large">选中文档以数组方式保存为1个文件</el-radio>
-            <el-radio label="more" size="large">选中文档作为独立文件保存到1个压缩文件</el-radio>
+            <el-radio label="one" size="large">以数组方式保存为压缩文件</el-radio>
+            <el-radio label="more" size="large">作为独立文件保存为压缩文件</el-radio>
           </el-radio-group>
         </el-form-item>
         <el-form-item label="叶子节点数" v-if="outType === 'excel'" class="form-item">
