c0ff4707dc867f77038cd33fa049683e197cae46
jasony62 <jasony62@qq.com>
2024-02-18 13:38:39 +0800

    避免启动时覆盖用户指定的settings.json文件

--- docker/start_all.sh
+++ docker/start_all.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
-# 后端推送服务端口（默认不打开推动端口）
-export UE_SETTINGS_BACK_PUSH_PORT=${UE_SETTINGS_BACK_PUSH_PORT:-\-1}
-
-# 生成默认的前端设置文件
-envsubst '$UE_SETTINGS_BACK_PUSH_PORT' < /usr/share/nginx/html/admin/settings.json.template > /usr/share/nginx/html/admin/settings.json
+# 检查是否已经存在settings.json文件，若无，根据环境变量生成默认文件
+SETTINGS_FILE="/usr/share/nginx/html/admin/settings.json"  
+if [ ! -f "$SETTINGS_FILE" ]; then  
+  # 后端推送服务端口（默认不打开推动端口）
+  export UE_SETTINGS_BACK_PUSH_PORT=${UE_SETTINGS_BACK_PUSH_PORT:-\-1}
+  # 生成默认的前端设置文件
+  envsubst '$UE_SETTINGS_BACK_PUSH_PORT' < /usr/share/nginx/html/admin/settings.json.template > /usr/share/nginx/html/admin/settings.json
+  
+  echo "根据环境变量，自动生成settings.json文件"
+fi
 
 # 启动nginx服务
 nohup /usr/local/bin/start_nginx.sh &>/dev/null &
