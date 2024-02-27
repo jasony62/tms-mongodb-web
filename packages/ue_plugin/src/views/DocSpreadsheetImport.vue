3fd942464660d75ac6d1527dcc69f94b0b5a9061
jasony62 <jasony62@qq.com>
2024-02-27 12:17:55 +0800

    支持集合导入excel数据时选择sheet页

--- packages/ue_plugin/src/views/DocSpreadsheetImport.vue
+++ packages/ue_plugin/src/views/DocSpreadsheetImport.vue
@@ -18,10 +18,15 @@
     <div v-if="operation === 'excel' || operation === 'json'">
       <el-form label-width="120px">
         <el-form-item>
-          <el-upload ref="upload" :action="''" :http-request="handleUpload" :file-list="fileList" :auto-upload="false"
+          <el-upload ref="upload" :action="''" :http-request="handleUpload" :file-list="fileList" :auto-upload="true"
             :limit="1" :on-change="handleChange">
             <el-button slot="trigger" type="primary">选取文件</el-button>
           </el-upload>
+        </el-form-item>
+        <el-form-item label="选择页" v-if="isUploadExcel">
+          <el-select v-model="selectedSheetName" placeholder="选择">
+            <el-option v-for="name in sheetNames" :key="name" :label="name" :value="name" />
+          </el-select>
         </el-form-item>
         <el-form-item label="数据起始行">
           <el-input-number v-model="startRow" :step="1" :min="1" step-strictly />
@@ -48,7 +53,7 @@
         </el-form-item>
         <el-divider />
         <el-form-item>
-          <el-button type="primary" @click="onExecute('upload')" :disabled="noUpload">执行</el-button>
+          <el-button type="primary" @click="onExecute('upload')" :disabled="!canSubmit">执行</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -72,94 +77,118 @@
 </template>
 
 <script setup lang="ts">
-import { computed, reactive, ref, toRaw, watch } from "vue"
+import { computed, reactive, ref, toRaw, watch } from 'vue'
 import * as XLSX from 'xlsx'
 import { ElMessage } from 'element-plus'
 
 const operation = ref('excel') // 要执行的操作
 const executed = ref(false)
-const responseContent = ref<string>("")
+const responseContent = ref<string>('')
 const fileList = ref([])
 const upload = ref<any>(null)
-const noUpload = ref(true)
+const noUploadFile = ref(true)
+const isUploadExcel = ref(false)
 const leafLevel = ref<number>(0)
 
 enum PluginWidgetAction {
-  Created = "Created",
-  Cancel = "Cancel",
-  Execute = "Execute",
-  Close = "Close",
+  Created = 'Created',
+  Cancel = 'Cancel',
+  Execute = 'Execute',
+  Close = 'Close',
 }
 
 interface PluginWidgetResult {
-  action: PluginWidgetAction;
-  result?: any;
-  handleResponse?: boolean;
-  applyAccessTokenField?: string; // 定用户输入中申请添加access_token的字段
-  reloadOnClose?: boolean; // 关闭部件后是否要刷新数据
+  action: PluginWidgetAction
+  result?: any
+  handleResponse?: boolean
+  applyAccessTokenField?: string // 定用户输入中申请添加access_token的字段
+  reloadOnClose?: boolean // 关闭部件后是否要刷新数据
 }
-
+// excel文件sheet页名称
+const sheetNames = ref([] as string[])
+const selectedSheetName = ref('')
 // 数据起始行号
 const startRow = ref(2)
 // 数据写入位置的最小值，覆盖写入时为1，插入写入时为0
-const minStartIndex = computed(() => writeOptions.overwrite ? 1 : 0)
+const minStartIndex = computed(() => (writeOptions.overwrite ? 1 : 0))
 /**
  * 数据写入设置
  */
 const writeOptions = reactive({
-  rebuild: false,// 重建自由表格
-  overwrite: false,// 覆盖已有数据，否则插入
-  startIndex: 0 // 数据写入位置
+  rebuild: false, // 重建自由表格
+  overwrite: false, // 覆盖已有数据，否则插入
+  startIndex: 0, // 数据写入位置
 })
 // 保证插入时，写入起始位置的最小值为1
-watch(() => writeOptions.overwrite, (newVal) => {
-  if (newVal === true && writeOptions.startIndex <= 1) {
-    writeOptions.startIndex = 1
+watch(
+  () => writeOptions.overwrite,
+  (newVal) => {
+    if (newVal === true && writeOptions.startIndex <= 1) {
+      writeOptions.startIndex = 1
+    }
   }
+)
+/**
+ * 检查是否满足提交条件
+ */
+const canSubmit = computed(() => {
+  if (noUploadFile.value === false) {
+    if (isUploadExcel.value === true) {
+      if (selectedSheetName.value) {
+        return true
+      }
+    } else {
+      return true
+    }
+  }
+  return false
 })
 
 // 调用插件的页面
-const Caller = window.parent;
-const message: PluginWidgetResult = { action: PluginWidgetAction.Created };
-Caller.postMessage(message, "*");
+const Caller = window.parent
+const message: PluginWidgetResult = { action: PluginWidgetAction.Created }
+Caller.postMessage(message, '*')
 
 /**接收结果*/
-window.addEventListener("message", (event) => {
-  const { data } = event;
-  const { response } = data;
+window.addEventListener('message', (event) => {
+  const { data } = event
+  const { response } = data
   if (response) {
-    noUpload.value = false
-    if (typeof response === "string") {
-      responseContent.value = response;
-    } else if (typeof response === "object") {
+    noUploadFile.value = false
+    if (typeof response === 'string') {
+      responseContent.value = response
+    } else if (typeof response === 'object') {
       if (response.filePath) {
         window.open(response.filePath)
       } else {
-        responseContent.value = JSON.stringify(response, null, 2);
+        responseContent.value = JSON.stringify(response, null, 2)
       }
     }
   }
-});
+})
 
 function handleChange(file: any, files: any) {
-  if (files.length) noUpload.value = false
+  if (files.length) noUploadFile.value = false
 }
 
 function setLevel(value: number) {
   if (Number.isNaN(value)) ElMessage.error('不支持的数据格式')
 }
 
-function handleUpload(req: any) {
-  noUpload.value = true
+let doSubmit: any = null
 
-  const reader = new FileReader();
+function handleUpload(req: any) {
+  const reader = new FileReader()
 
   //将文件以二进制形式读入页面
   const fileType = req.file.type
   if (fileType === 'application/json') {
-    reader.readAsText(req.file);
-  } else if (fileType === 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet') {
-    reader.readAsArrayBuffer(req.file);
+    reader.readAsText(req.file)
+  } else if (
+    fileType ===
+    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+  ) {
+    reader.readAsArrayBuffer(req.file)
   } else {
     ElMessage.error('不支持的文件格式')
     return
@@ -167,69 +196,90 @@ function handleUpload(req: any) {
 
   reader.onload = function (event: any) {
     const fileData = event.target.result
-    let contentType = ''
-    let rowsJson
-    if (fileType === 'application/json') {
-      rowsJson = JSON.parse(fileData)
-      contentType = 'json'
-    } else {
-      const wb = XLSX.read(fileData, { type: 'binary' })
-      const firstSheetName = wb.SheetNames[0]
-      const sh = wb.Sheets[firstSheetName]
-      rowsJson = XLSX.utils.sheet_to_json(sh, { header: 1 })
-      if (Array.isArray(rowsJson) && rowsJson.length) {
-        const deleteCount = startRow.value - 1
-        if (deleteCount > 0 && deleteCount <= rowsJson.length) {
-          rowsJson.splice(0, deleteCount)
+    /**
+     * 获取文件信息
+     */
+    const { contentType, rowsJson, wb } = (() => {
+      let contentType = ''
+      let rowsJson, wb
+      if (fileType === 'application/json') {
+        rowsJson = JSON.parse(fileData)
+        contentType = 'json'
+        isUploadExcel.value = false
+      } else {
+        wb = XLSX.read(fileData, { type: 'binary' })
+        sheetNames.value.push(...wb.SheetNames)
+        contentType = 'aoa'
+        isUploadExcel.value = true
+      }
+      return { contentType, rowsJson, wb }
+    })()
+    /**
+     * 等待执行的提交操纵
+     */
+    doSubmit = () => {
+      if (!Caller) return
+      let content // 要提交的内容
+      if (contentType === 'aoa' && wb) {
+        const sh = wb.Sheets[selectedSheetName.value]
+        content = XLSX.utils.sheet_to_json(sh, { header: 1 })
+        if (Array.isArray(content) && content.length) {
+          const deleteCount = startRow.value - 1
+          if (deleteCount > 0 && deleteCount <= content.length) {
+            content.splice(0, deleteCount)
+          }
         }
+      } else if (contentType === 'json' && rowsJson) {
+        content = rowsJson
       }
-      contentType = 'aoa'
-    }
-
-    if (Caller && contentType && rowsJson && typeof rowsJson === 'object') {
-      const message: PluginWidgetResult = {
-        action: PluginWidgetAction.Execute,
-        result: { content: JSON.stringify(rowsJson), contentType, writeOptions: toRaw(writeOptions) },
-        applyAccessTokenField: "url",
-      }
-      try {
-        // 给调用方发送数据
-        Caller.postMessage(message, "*");
-        executed.value = true;
-      } catch (e) {
-        console.log("未知错误", e);
+      if (contentType && content && typeof content === 'object') {
+        const message: PluginWidgetResult = {
+          action: PluginWidgetAction.Execute,
+          result: {
+            content: JSON.stringify(content),
+            contentType,
+            writeOptions: toRaw(writeOptions),
+          },
+          applyAccessTokenField: 'url',
+        }
+        try {
+          // 给调用方发送数据
+          Caller.postMessage(message, '*')
+          executed.value = true
+        } catch (e) {
+          console.log('未知错误', e)
+        }
       }
     }
   }
 }
 
 function onExecute(action: string) {
+  if (!Caller) return
   if (action === 'upload') {
-    if (!noUpload.value) upload.value?.submit()
+    if (doSubmit) doSubmit()
   } else if (action === 'download') {
-    if (Caller) {
-      const message: PluginWidgetResult = {
-        action: PluginWidgetAction.Execute,
-        result: { action: 'download', leafLevel: leafLevel.value },
-        handleResponse: true
-      }
-      try {
-        // 给调用方发送数据
-        Caller.postMessage(message, "*");
-        executed.value = true;
-      } catch (e) {
-        console.log("未知错误", e);
-      }
+    const message: PluginWidgetResult = {
+      action: PluginWidgetAction.Execute,
+      result: { action: 'download', leafLevel: leafLevel.value },
+      handleResponse: true,
+    }
+    try {
+      // 给调用方发送数据
+      Caller.postMessage(message, '*')
+      executed.value = true
+    } catch (e) {
+      console.log('未知错误', e)
     }
   } else {
-    console.log('不识别的操作指令');
+    console.log('不识别的操作指令')
   }
 }
 
 function onCancel() {
   if (Caller) {
-    const message: PluginWidgetResult = { action: PluginWidgetAction.Cancel };
-    Caller.postMessage(message, "*");
+    const message: PluginWidgetResult = { action: PluginWidgetAction.Cancel }
+    Caller.postMessage(message, '*')
   }
 }
 
@@ -238,8 +288,8 @@ function onClose() {
     const message: PluginWidgetResult = {
       action: PluginWidgetAction.Close,
       reloadOnClose: true,
-    };
-    Caller.postMessage(message, "*");
+    }
+    Caller.postMessage(message, '*')
   }
 }
 </script>
